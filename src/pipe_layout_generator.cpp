dfee6284efb550047157fb95667eb33e8a5d0e86
Wang Tai <i@wangtai.me>
2024-10-21 16:34:34 +0800

    Add pipe layout generation algorithm and update data structures
    Integrated the pipe layout generation algorithm in `pipe_layout_generator.cpp` and added corresponding header. Modified the `generatePipePlan` and `planToJson` functions to handle new HeatingDesign structure. Updated `helper.h` to include new data structures and adjusted `main.cpp` accordingly.

---
+++ src/pipe_layout_generator.cpp
@@ -0,0 +1,10 @@
+#include "pipe_layout_generator.h"
+#include "helper.h"
+
+CollectorCoil generatePipeLayout(const HouseType& houseType, const WebData& webData) {
+    CollectorCoil collectorCoil;
+
+    // Implement the pipe layout generation algorithm here
+
+    return collectorCoil;
+}
