d8f1a07dbb9b24d909c19802992edc21f408a428
Wang Tai <i@wangtai.me>
2024-10-22 15:26:02 +0800

    Create new unit tests and rename sample test file
    Added a new test file, `test_pipe_layout_generator.cpp`, with a simple test case. Renamed `test_sample.cpp` to `test_main.cpp` and cleaned up formatting.

---
+++ tests/test_pipe_layout_generator.cpp
@@ -0,0 +1,8 @@
+// tests/test_pipe_layout_generator.cpp
+#include <gtest/gtest.h>
+#include "../include/pipe_layout_generator.hpp"
+
+
+TEST(PipeLayoutGeneratorTest, Test1) {
+    EXPECT_EQ(1, 1);
+}
