969d43cfce3c3455d3cf41793638ef8c055cb87b
Junjie Fang <julyfun.collect@outlook.com>
2024-10-27 13:53:31 +0800

    M1 tree to pipe points algorithm in C++ (#1)
    * Feat: tree2pipe cpp version; fix bugs of son adding, direction and API
    
    * test: add test for tree2pipe
    
    * readme and gitignore
    
    * Refactor: Delete main function in algorithm file; add CMake configuration for Eigen3 and Matplot++

--- README.md
+++ README.md
@@ -4,6 +4,15 @@
 
 ## 安装
 
+依赖:
+
+```
+brew install opencv
+brew install eigen
+brew install jsoncpp@1.9.6
+brew install matplotplusplus
+```
+
 确保您的系统已安装 CMake、OpenCV 和 JsonCpp。然后执行以下命令：
    ```sh
 chmod +x scripts/build.sh
@@ -16,6 +25,22 @@ chmod +x scripts/build.sh
    ```sh
 ./build/iad <ARDesign.json路径> <inputData.json路径>
    ```
+
+## 算法
+
+- [x] Json parse 
+    - 平面图和数学约束条件
+- 平面图遗传算法
+    - 划分为面积接近的区域，每个区域由 `M1` 结点组成树形图，`M1` 结点为单点或矩形，带约束条件
+- [x] 路径生成
+    - 将每个 `M1` 树转换为管道路径
+    - [ ] 调整
+- 交付路径
+    - Web
+
+## Todo
+
+- 多边形
 
 ## 贡献
 欢迎贡献代码，请遵循以下指南...
---
+++ include/tree2pipe.hpp
@@ -0,0 +1,111 @@
+#ifndef TREE2PIPE_H
+#define TREE2PIPE_H
+
+#include <iostream>
+#include <memory>
+#include <optional>
+
+#include <Eigen/Core>
+
+namespace tree2pipe {
+
+using std::array;
+using std::cout;
+using std::enable_shared_from_this;
+using std::endl;
+using std::make_shared;
+using std::optional;
+using std::pair;
+using std::shared_ptr;
+using std::vector;
+
+using Eigen::Vector2d;
+using Eigen::Vector3d;
+
+struct Node {
+public:
+    Vector2d pos;
+    double subtree_width;
+    vector<shared_ptr<Node>> sons;
+
+    Node(
+        const Vector2d& pos,
+        const double subtree_width = 0,
+        const vector<shared_ptr<Node>>& sons = {}
+    ):
+        pos(pos),
+        subtree_width(subtree_width),
+        sons(sons) {}
+};
+
+struct CfgM1 {
+    optional<Vector2d> dir;
+    optional<double> w;
+};
+
+// Abstract base class
+class M1: public enable_shared_from_this<M1> {
+public:
+    virtual Vector2d get_pos() const = 0;
+    virtual shared_ptr<Node> to_tree(const CfgM1& anc_recommend) = 0;
+    virtual ~M1() = default;
+};
+
+struct RectM1Son {
+    int eid;
+    shared_ptr<M1> m1;
+};
+
+class RectM1: public M1 {
+private:
+    Vector2d p1, p3;
+    CfgM1 cfg;
+    vector<RectM1Son> sons;
+
+public:
+    RectM1(
+        const Vector2d& p1,
+        const Vector2d& p3,
+        const CfgM1& cfg = {},
+        const vector<RectM1Son>& sons = {}
+    ):
+        p1(p1),
+        p3(p3),
+        cfg(cfg),
+        sons(sons) {}
+
+    Vector2d get_pos() const override {
+        return this->p1;
+    }
+
+    shared_ptr<Node> to_tree(const CfgM1& anc_recommend) override;
+};
+
+class NodeM1: public M1 {
+private:
+    Vector2d pos;
+    CfgM1 cfg;
+    vector<shared_ptr<M1>> sons;
+
+public:
+    NodeM1(const Vector2d& pos, const CfgM1& cfg = {}, const vector<shared_ptr<M1>>& sons = {}):
+        pos(pos),
+        cfg(cfg),
+        sons(sons) {}
+
+    Vector2d get_pos() const override {
+        return pos;
+    }
+
+    shared_ptr<Node> to_tree(const CfgM1& anc_recommend) override;
+};
+
+vector<Vector2d> lines_to_points(const vector<Vector3d>& lines, bool verbose = false);
+
+void plot_points_linked(const std::vector<Vector2d>& pts);
+
+vector<Vector2d> tree_to_points(shared_ptr<Node> root, Vector2d up_dir = Vector2d::Zero());
+
+} // namespace tree2pipe
+
+#endif
