630d80e290133ebf9a4c90925f3345a018cc197e
Wang Tai <i@wangtai.me>
2024-11-25 18:47:12 +0800

    Refactor JSON parsing and heating design generation
    Split `json_parser` functionality into `input_parser` and `output_parser` for better separation of concerns. Moved shared types to a new `types.hpp` header, updated includes and references accordingly.

--- include/core/pipeline/pipe_layout_generator.hpp
+++ include/core/pipeline/pipe_layout_generator.hpp
@@ -1,7 +1,9 @@
 #ifndef PIPE_LAYOUT_GENERATOR_H
 #define PIPE_LAYOUT_GENERATOR_H
 
-#include "core/parsing/json_parser.hpp"
+#include "types/heating_design_structures.hpp"
+#include "types/ar_design_structures.hpp"
+#include "types/input_data_structures.hpp"
 
 CollectorCoil generatePipeLayout(const HouseType& houseType, const WebData& webData);
 
