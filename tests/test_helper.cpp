49908c72f9bf7e0f9b6b486ba212fe6f2be319da
Wang Tai <i@wangtai.me>
2024-12-17 08:23:31 +0800

    Update file paths in test_helper.cpp for consistency with directory structure
    - Changed file paths in test cases to use relative paths that correctly point to the example JSON files.
    - Commented out the old file paths to maintain a record of previous references.
    
    These adjustments ensure that the tests can locate the necessary files without errors, improving the reliability of the test suite.

--- tests/test_helper.cpp
+++ tests/test_helper.cpp
@@ -113,7 +113,8 @@ TEST_F(ParseARDesignTest, ParsesActualARDesignFile) {
     getcwd(cwd, sizeof(cwd));
     // Read file content
     // std::string filePath = "../../example/ARDesign-min.json";
-    std::string filePath = "../example/ARDesign2.json";
+    // std::string filePath = "../example/ARDesign2.json";
+    std::string filePath = "../../example/ARDesign2.json";
     // std::string filePath = "../example/ARDesign01.json";
     std::ifstream file(filePath);
     if (!file.is_open()) {
@@ -166,7 +167,8 @@ TEST_F(ParseARDesignTest, ParsesActualARDesignFile01) {
     // Read file content
     // std::string filePath = "../../example/ARDesign-min.json";
     // std::string filePath = "../example/ARDesign2.json";
-    std::string filePath = "../example/ARDesign02.json";
+    // std::string filePath = "../example/ARDesign02.json";
+    std::string filePath = "../../example/ARDesign02.json";
     std::ifstream file(filePath);
     if (!file.is_open()) {
         std::cerr << "Current working directory: " << cwd << std::endl;
